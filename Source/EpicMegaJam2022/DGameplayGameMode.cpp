19f66c001a9a077aeec3b971e70d2da30d614b8b
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2022-08-26 12:30:06 +0200

    Added in logic to spawn a given number of pawns

--- Source/EpicMegaJam2022/DGameplayGameMode.cpp
+++ Source/EpicMegaJam2022/DGameplayGameMode.cpp
@@ -3,3 +3,36 @@
 
 #include "DGameplayGameMode.h"
 
+#include "DCharacter.h"
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+void ADGameplayGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnPlayerPawns();
+}
+
+void ADGameplayGameMode::SpawnPlayerPawns()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (Characters)
+		{
+			// Get all of the player starts in the level
+			TArray<AActor*> PlayerStarts;
+			UGameplayStatics::GetAllActorsOfClass(World ,APlayerStart::StaticClass(), PlayerStarts);
+
+			// Spawn the specified number of characters until it reaches the total number of spawn points available in the world
+			for (int i = 0; i < NumberOfCharactersToSpawn; i++)
+			{
+				if (i + 1 <= PlayerStarts.Num())
+				{
+					World->SpawnActor(Characters, &PlayerStarts[i]->GetTransform());	
+				}
+				else break;
+			}
+		}
+	}
+}
--- Source/EpicMegaJam2022/DGameplayGameMode.h
+++ Source/EpicMegaJam2022/DGameplayGameMode.h
@@ -3,15 +3,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
+//#include "DCharacter.h"
 #include "GameFramework/GameModeBase.h"
 #include "DGameplayGameMode.generated.h"
 
 /**
  * 
  */
+
+class ADCharacter;
+
 UCLASS()
 class EPICMEGAJAM2022_API ADGameplayGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected:
+
+	// A custom function in order to spawn a given number of local ADCharacters
+	void SpawnPlayerPawns();
+	
+public:
+
+	// The type of character we want to spawn
+	UPROPERTY(EditDefaultsOnly, Category = "Spawning")
+	TSubclassOf<ADCharacter> Characters;
+
+	// The number of characters we want to spawn
+	UPROPERTY(EditDefaultsOnly, Category = "Spawning")
+	int NumberOfCharactersToSpawn;
+
+	// ¬ Begin APlayerController interface
+	virtual void BeginPlay() override;
+	// ¬ End APlayerController interface
 };

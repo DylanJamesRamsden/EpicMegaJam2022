4205c851299fd115f7603e50bd3bff642332d2a4
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2022-08-26 13:01:19 +0200

    Bound our cycle function to our input action and we can now cycle between pawns

--- Source/EpicMegaJam2022/DPlayerController.cpp
+++ Source/EpicMegaJam2022/DPlayerController.cpp
@@ -12,14 +12,7 @@ void ADPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// @TODO Can update this to only grab camera that have a 'Level Camera' tag
-	if (AActor* FoundCameraActor = UGameplayStatics::GetActorOfClass(this, ACameraActor::StaticClass()))
-	{
-		LevelCamera = Cast<ACameraActor>(FoundCameraActor);
-		ensure(LevelCamera);
-		
-		SetViewTarget(LevelCamera);
-	}
+	SetLevelCameraAsViewTarget();
 
 	// Just grabbing and storing all of the available ADCharacters in the world
 	// NB! Haven't worked with a non-multiplayer game in a while, but going to assume there are no timing issues
@@ -29,4 +22,50 @@ void ADPlayerController::BeginPlay()
 		AvailableCharacters.Add(*It);
 	}
 
+}
+
+void ADPlayerController::SetLevelCameraAsViewTarget()
+{
+	// @TODO Can update this to only grab camera that have a 'Level Camera' tag
+	if (LevelCamera)
+	{
+		SetViewTarget(LevelCamera);
+	}
+	else
+	{
+		if (AActor* FoundCameraActor = UGameplayStatics::GetActorOfClass(this, ACameraActor::StaticClass()))
+		{
+			LevelCamera = Cast<ACameraActor>(FoundCameraActor);
+			ensure(LevelCamera);
+		
+			SetViewTarget(LevelCamera);
+		}
+	}
+}
+
+void ADPlayerController::CyclePossessedPawn()
+{
+	if (ensure(AvailableCharacters.Num() > 0))
+	{
+		if (PossessedIndex < AvailableCharacters.Num() - 1)
+		{
+			PossessedIndex++;
+		}
+		else PossessedIndex = 0;
+
+		Possess(AvailableCharacters[PossessedIndex]);
+
+		// Kind of annoying we need to recall this, but just ensures we keep our view target as our LevelCamera
+		SetLevelCameraAsViewTarget();	
+	}
+}
+
+void ADPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (InputComponent)
+	{
+		InputComponent->BindAction("CyclePawn", IE_Pressed, this, &ADPlayerController::CyclePossessedPawn);
+	}
 }
--- Source/EpicMegaJam2022/DPlayerController.h
+++ Source/EpicMegaJam2022/DPlayerController.h
@@ -21,12 +21,23 @@ protected:
 	UPROPERTY()
 	ACameraActor* LevelCamera;
 
+	// Sets our LevelCamera as our current view target
+	void SetLevelCameraAsViewTarget();
+
+	// Stores all of the available pawns we can possess
 	UPROPERTY()
 	TArray<ADCharacter*> AvailableCharacters;
+
+	// Keeps track of the index of which pawn we are currently possessing
+	int32 PossessedIndex;
+
+	// Cycles between our available pawns, based on our PossessedIndex
+	void CyclePossessedPawn();
 public:
 
 	// ¬ Begin APlayerController interface
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	// ¬ End APlayerController interface
 	
 };

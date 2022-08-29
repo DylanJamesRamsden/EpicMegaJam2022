9f6e80370e777809fb255115b093e784bd65752e
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2022-08-29 22:01:51 +0200

    Created ladder base class and BP

---
+++ Source/EpicMegaJam2022/DLadder.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DPushable.h"
+#include "DLadder.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICMEGAJAM2022_API ADLadder : public ADPushable
+{
+	GENERATED_BODY()
+
+protected:
+
+	
+};
